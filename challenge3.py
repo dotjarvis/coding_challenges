e399ac42e0ac0c330e419d0ae5b0b57eaa1c2885
jarvis_codes <106377265+dotjarvis@users.noreply.github.com>
2022-12-13 22:44:01 +0300

    Added one challenge for DEC 13
    Due to limited time for more room of revision, I managed one challenge

--- challenge3.py
+++ challenge3.py
@@ -576,3 +576,38 @@
 # print(censor_string("Today is a Wednesday!", ["Today", "a"], "-"))
 # print(censor_string("The cow jumped over the moon.", ["cow", "over"], "*"))
 # print(censor_string("Why did the chicken cross the road ?", ["Did", "chicken", "road"], "*"))
+
+
+# TUE, DEC 13 2022
+
+
+# QUESTION 1
+# Given a list of integers representing the color of each sock,
+# determine how many pairs of socks with matching colors there are.
+# For example, there are 7 socks with colors [1, 2, 1, 2, 1, 3, 2].
+# There is one pair of color 1 and one of color 2.
+# There are three odd socks left, one of each color. The number of pairs is 2.
+#
+# Create a function that returns an integer representing the number of
+# matching pairs of socks that are available.
+# Examples
+# sock_merchant([10, 20, 20, 10, 10, 30, 50, 10, 20]) ➞ 3
+# sock_merchant([50, 20, 30, 90, 30, 20, 50, 20, 90]) ➞ 4
+# sock_merchant([]) ➞ 0
+
+# # SOLUTION
+# def sock_merchant(lst):
+#     unique_array = []
+#     total = 0
+#     for color in lst:
+#         if color not in unique_array:
+#             unique_array.append(color)
+#             number = lst.count(color)
+#             total += number // 2
+# 
+#     return total
+# 
+# 
+# print(sock_merchant([10, 20, 20, 10, 10, 30, 50, 10, 20]))
+# print(sock_merchant([50, 20, 30, 90, 30, 20, 50, 20, 90]))
+# print(sock_merchant([]))
