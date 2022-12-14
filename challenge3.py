ffb684d06b491378781bcc33345a6169e1dcfa86
jarvis_codes <106377265+dotjarvis@users.noreply.github.com>
2022-12-14 14:44:18 +0300

    Added one more challenge for Dec 14
    Hopefully i'll be done with my exams tomorrow and do  more challenges daily

--- challenge3.py
+++ challenge3.py
@@ -604,10 +604,59 @@
 #             unique_array.append(color)
 #             number = lst.count(color)
 #             total += number // 2
-# 
+#
 #     return total
-# 
-# 
+#
+#
 # print(sock_merchant([10, 20, 20, 10, 10, 30, 50, 10, 20]))
 # print(sock_merchant([50, 20, 30, 90, 30, 20, 50, 20, 90]))
 # print(sock_merchant([]))
+
+
+# WED DEC, 14 2022
+
+
+# QUESTION 1
+
+# Given a function that accepts unlimited arguments,
+# check and count how many data types are in those arguments.
+# Finally return the total in a list.
+# List order is:
+# [int, str, bool, list, tuple, dictionary]
+# Examples
+# count_datatypes(1, 45, "Hi", False) ➞ [2, 1, 1, 0, 0, 0]
+# count_datatypes([10, 20], ("t", "Ok"), 2, 3, 1) ➞ [3, 0, 0, 1, 1, 0]
+# count_datatypes("Hello", "Bye", True, True, False, {"1": "One", "2": "Two"}, [1, 3], {"Brayan": 18}, 25, 23) ➞ [2, 2, 3, 1, 0, 2]
+# count_datatypes(4, 21, ("ES", "EN"), ("a", "b"), False, [1, 2, 3], [4, 5, 6]) ➞ [2, 0, 1, 2, 2, 0]
+# Notes
+# If no arguments are given, return [0, 0, 0, 0, 0, 0]
+
+
+# # SOLUTION
+# def count_datatypes(*args):
+#     if not bool(args):
+#         return [0, 0, 0, 0, 0, 0]
+#     int_type = str_type = bool_type = list_type = tuple_type = dictionary_type = 0
+# 
+#     for data_type in args:
+#         if type(data_type) == int:
+#             int_type += 1
+#         elif type(data_type) == str:
+#             str_type += 1
+#         elif type(data_type) == bool:
+#             bool_type += 1
+#         elif type(data_type) == list:
+#             list_type += 1
+#         elif type(data_type) == tuple:
+#             tuple_type += 1
+#         elif type(data_type) == dict:
+#             dictionary_type += 1
+# 
+#     return [int_type, str_type, bool_type, list_type, tuple_type, dictionary_type]
+# 
+# 
+# print(count_datatypes(1, 45, "Hi", False))
+# print(count_datatypes([10, 20], ("t", "Ok"), 2, 3, 1))
+# print(count_datatypes("Hello", "Bye", True, True, False, {"1": "One", "2": "Two"}, [1, 3], {"Brayan": 18}, 25, 23))
+# print(count_datatypes(4, 21, ("ES", "EN"), ("a", "b"), False, [1, 2, 3], [4, 5, 6]))
+# print(count_datatypes())
